5e2a5c25e7182faeb450b1a939ca06971292f28e
paolaguedes <53832972+paolaguedes@users.noreply.github.com>
2021-09-21 10:03:50 -0300

    js

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+let coracoes = document.getElementsByClassName("far")
+
+for(let i=0; i<coracoes.length;i++){
+  coracoes[i].onclick = function(event){
+     event.currentTarget.classList.toggle("fas");
+     //toggle mostra e esconde a classe
+     //quando clicado mostra a classe fas e qnd clica dnv esconde a classe fas
+  }
+}
 
 
 function playAudio() {
